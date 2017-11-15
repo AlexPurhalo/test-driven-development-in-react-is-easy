20b3e905d4918ddff286d0da75489fe5d67eef1e
AlexPurhalo <alexpurhalo@gmail.com>
2017-11-15 21:32:08 +0200

    Prop types verifying

---
+++ src/components/ProductList.js
@@ -0,0 +1,15 @@
+import React     from 'react'
+import PropTypes from 'prop-types'
+
+const ProductList = (props) => (
+	<div>
+		ProductList
+	</div>
+)
+
+
+ProductList.propTypes = {
+	products: PropTypes.array.isRequired
+};
+
+export default ProductList
